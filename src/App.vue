ee2817c81284d84906a6cdfa0fdc917a1e445213
Yannick Ferire <yannick@bureau347.com>
2018-05-31 15:20:07 +0200

    ChooseNation Component creation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ChooseNation />
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/*import HelloWorld from './components/HelloWorld.vue'*/
+import ChooseNation from './components/ChooseNation.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    /*HelloWorld*/
+    ChooseNation
   }
 }
 </script>
--- src/components/ChooseNation.vue
+++ src/components/ChooseNation.vue
@@ -1,15 +1,10 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+    <h1>Follow the path of you nation at World Cup! 🏆</h1>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'ChooseNation'
 }
 </script>
 

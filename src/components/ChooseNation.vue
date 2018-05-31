8de5a571788ca37aede6bdebf1fa10800e4cef60
Yannick Ferire <yannick@bureau347.com>
2018-05-31 15:55:26 +0200

    Add SCSS support

--- src/components/ChooseNation.vue
+++ src/components/ChooseNation.vue
@@ -1,27 +1,33 @@
 <template>
-    <h1>Follow the path of you nation at World Cup! 🏆</h1>
+    <div class="choose-nation">
+        <h1>
+            Follow the path of 
+            <div class="select-wrapper">
+                <select v-model="choosedNation">
+                    <option disabled value="">your nation</option>
+                    <option>🇧🇪 Belgium</option>
+                    <option>🇵🇦 Panama</option>
+                    <option>🇹🇳 Tunisia</option>
+                    <option>🏴 England󠁧󠁢󠁥󠁮󠁧󠁿</option>
+                </select> 
+            </div>
+            at World Cup! 🏆
+        </h1>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'ChooseNation'
+  name: 'ChooseNation',
+  data: function() {
+      return {
+        choosedNation: ''
+      };
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+@import '../main.scss';
 </style>
